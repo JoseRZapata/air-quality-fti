5fada315086a3e3b305d307aa232fe23abea7e2e
Jose R. Zapata <jjrzg@hotmail.com>
2025-05-09 23:23:51 -0500

    feat: update config.py remove function

--- src/config.py
+++ src/config.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 from typing import Literal
 
@@ -7,13 +6,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class HopsworksSettings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8", extra="ignore"
-    )
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
     MLFS_DIR: Path = Path(__file__).parent
 
-    # For hopsworks.login(), set as environment variables if they are not already set as env variables
+    # For hopsworks.login(),
+    # set as environment variables if they are not already set as env variables
     HOPSWORKS_API_KEY: SecretStr | None = None
     HOPSWORKS_PROJECT: str | None = None
     HOPSWORKS_HOST: str | None = None
@@ -28,18 +26,3 @@ class HopsworksSettings(BaseSettings):
     # Inference
     RANKING_MODEL_TYPE: Literal["ranking", "llmranking"] = "ranking"
     CUSTOM_HOPSWORKS_INFERENCE_ENV: str = "custom_env_name"
-
-    def model_post_init(self, __context):
-        """Runs after the model is initialized."""
-        print("HopsworksSettings initialized!")
-        if os.getenv("HOPSWORKS_API_KEY") == None:
-            if self.HOPSWORKS_API_KEY is not None:
-                os.environ["HOPSWORKS_API_KEY"] = (
-                    self.HOPSWORKS_API_KEY.get_secret_value()
-                )
-        if os.getenv("HOPSWORKS_PROJECT") == None:
-            if self.HOPSWORKS_PROJECT is not None:
-                os.environ["HOPSWORKS_PROJECT"] = self.HOPSWORKS_PROJECT
-        if os.getenv("HOPSWORKS_HOST") == None:
-            if self.HOPSWORKS_HOST is not None:
-                os.environ["HOPSWORKS_HOST"] = self.HOPSWORKS_HOST
